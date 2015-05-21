e036f9f68101373e7e7924c70b8f967d2a54d732
David Fu <david.fu.zju.dev@gmail.com>
2015-05-21 23:49:47 +0800

    init project

---
+++ like/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  like
+//
+//  Created by David Fu on 5/21/15.
+//  Copyright (c) 2015 LIKE Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
